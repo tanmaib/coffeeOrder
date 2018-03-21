62998d52d832fb1ff08b1ad2f9bfc6a5ca1e5ff0
tanmaibhargav <tanmaibhargav@gmail.com>
2018-03-22 00:06:19 +0530

    Add dimens and string files. Create TextView global variable.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,11 +22,16 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     int noofcoffee = 0;
+    TextView quantityTextView, priceTextView, priceTextViewS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        priceTextView = (TextView) findViewById(R.id.price_text_view);
+
     }
 
     /**
@@ -58,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        //TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
@@ -66,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        //TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
        /** TextView textView = (TextView) findViewById(R.id.price_text_view);
         textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.icon, 0, 0, 0);*/
@@ -76,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        //TextView priceTextViewS = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
     }
 }
